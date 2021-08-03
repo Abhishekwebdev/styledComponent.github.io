0bea9bbb1702533adda1a56a79838691cbc1e115
Abhishek Patel <Abhi882731@gmail.com>
2021-08-02 22:54:57 -0700

    Updates

---
+++ precache-manifest.85e6e5f613ceda05dd94f22a46e3ad3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ffa737245b2458194865d2b1b4ada40",
+    "url": "/styledComponent.github.io/index.html"
+  },
+  {
+    "revision": "4027614811ef176974e8",
+    "url": "/styledComponent.github.io/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fe2a2d467cb367f1669d",
+    "url": "/styledComponent.github.io/static/js/2.8fd41d7d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/styledComponent.github.io/static/js/2.8fd41d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4027614811ef176974e8",
+    "url": "/styledComponent.github.io/static/js/main.4195e4e8.chunk.js"
+  },
+  {
+    "revision": "3b0fb7d4901342573a69",
+    "url": "/styledComponent.github.io/static/js/runtime-main.e33d2b12.js"
+  }
+]);
